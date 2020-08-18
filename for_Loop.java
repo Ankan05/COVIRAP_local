dda420eeaf6f636cceac1b8076180efc367ba22c
Ankan05 <69791909+Ankan05@users.noreply.github.com>
2020-08-18 19:25:34 +0530

    updated for_Loop with new algo

--- for_Loop.java
+++ for_Loop.java
@@ -2,6 +2,7 @@ public class for_Loop{
 	Public static void main(String[] args){
 		for(int i=0;i<4;i++){ 
 			System.out.println("Hello World");
-		}	
+		}
+			System.out.println("done with for loop");
 	}
 }
